5dd4bb7ea91bd885bfbab67e034ae98b0db1c98a
bosovskyi <sergii@takeoff.com>
2021-09-28 11:03:22 +0300

    add terraform enable projects

---
+++ go-pets/terraform/main.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.53"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project
+}
+
+locals {
+  service_name   = "go-pets"
+
+  deployment_name = "go-pets"
+  pets_worker_sa  = "serviceAccount:${google_service_account.pets_worker.email}"
+}
+
+resource "google_project_service" "run" {
+  service = "run.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "iam" {
+  service = "iam.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "cloudbuild" {
+  service = "cloudbuild.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "datastore" {
+  service = "datastore.googleapis.com"
+  disable_on_destroy = false
+}
+
+# Create a service account
+resource "google_service_account" "pets_worker" {
+  account_id   = "pets-worker"
+  display_name = "Pets Worker SA"
+}
+
+# Set permissions
+resource "google_project_iam_binding" "service_permissions" {
+  role       = "run.invoker"
+  members    = [local.pets_worker_sa]
+  depends_on = [google_service_account.pets_worker]
+}
